a35c346e31c552b4cba22667ad3a6347555d9843
Snaflus <casper.riis.lie.nielsen@gmail.com>
2022-12-05 14:00:48 +0000

    Riverpod research done

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+final counterProvider = StateNotifierProvider((ref) {
+  return Counter();
+});
+
+class Counter extends StateNotifier<int> {
+  Counter() : super(0);
+
+  void increment() => state++;
 }
 
 class MyApp extends StatelessWidget {
@@ -28,40 +39,63 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ProgressBar extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var count = int.tryParse(ref.watch(counterProvider).toString());
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    return Column(
+      children: [
+        Text('Button clicks out of 10'),
+        LinearProgressIndicator(value: count! / 10),
+      ],
+    );
   }
+}
 
+class CounterDisplay extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var count = ref.watch(counterProvider).toString();
+
+    return Column(
+      children: [
+        const Text(
+          'You have pushed the button this many times:',
+        ),
+        Text(
+          count,
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ],
+    );
+  }
+}
+
+class CounterButton extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FloatingActionButton(
+      onPressed: ref.read(counterProvider.notifier).increment,
+      tooltip: 'Increment',
+      child: const Icon(Icons.add),
+    );
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[CounterDisplay(), ProgressBar()],
+          ),
+        ),
+        floatingActionButton: CounterButton());
   }
 }
