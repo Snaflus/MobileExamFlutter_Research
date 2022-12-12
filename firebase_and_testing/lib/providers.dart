9e702e706754da46be39b9f7d433847f4cfe9d1b
Snaflus <casper.riis.lie.nielsen@gmail.com>
2022-12-12 15:34:11 +0000

    Update providers.dart

--- firebase_and_testing/lib/providers.dart
+++ firebase_and_testing/lib/providers.dart
@@ -2,10 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final firebaseAuthProvider =
-Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
+    Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
 
 final authStateChangesProvider = StreamProvider.autoDispose<User?>(
-        (ref) => ref.watch(firebaseAuthProvider).authStateChanges());
+    (ref) => ref.watch(firebaseAuthProvider).authStateChanges());
 
 final counterProvider = StateNotifierProvider((ref) {
   return Counter();
